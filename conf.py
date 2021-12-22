52504d8eb72cee85e121cd30e2bc36e4eed0821c
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2021-12-22 12:42:16 +0000

    Nikola auto commit.
    Nikola version: 8.1.3

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "GeNN Team"  # (translatable)
 BLOG_TITLE = "GeNN"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "http://genn-team.github.io/genn/"
+SITE_URL = "http://genn-team.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "http://genn-team.github.io/genn/"
