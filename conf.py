5c24950c6231cae80fa5074d2238c1f50c4e0a3a
neworderofjamie <neworderofjamie@gmail.com>
2022-05-20 15:39:37 +0100

    added home page for tutorials

--- conf.py
+++ conf.py
@@ -141,6 +141,7 @@ TRANSLATIONS_PATTERN = '{path}.{lang}.{ext}'
 NAVIGATION_LINKS = {
     DEFAULT_LANG: (
         ("https://genn-team.github.io/genn/documentation/4/html/index.html", "Documentation"),
+        ("/tutorials.html", "Tutorials"),
         ("/blog/","Latest Blog"), 
         ("/archive.html", "Blog Archive"),
         ("/categories/", "Blog Tags"),
